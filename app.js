788a0ccd1b963a552fb7ab95c9221d58dab2e722
Pawel Slomka <pawel.slomka@siili.fi>
2015-10-02 15:34:51 +0200

    Create middleware

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ var express = require('express');
 var app = express();
 
 var greeting = require('./server/basic/greeting');
+var greetingMiddleware = require('./server/basic/greeting-middleware');
 
-app.get('/', function(req, res){
+app.get('/greet', function(req, res){
     var name = req.query.name;
     var message = greeting(name);
 
     res.status(418)
         .end(message);
 });
+
+app.get('/greet2', greetingMiddleware);
+
 app.listen(9000);
